430cc22d2dd8940e7fe3786c794027bcb73b10c0
Bryan Goss <bcgoss@gmail.com>
2017-03-31 17:31:39 -0700

    Add views for projects and posts. Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.find_by email: 'bcgoss@gmail.com'
+unless user
+  user = User.new email: 'bcgoss@gmail.com', password: 'password' 
+  if user.save
+    puts 'User saved, creating projects'
+  else
+    puts user.errors.full_messages.join(', ')
+  end
+else
+  puts 'User found'
+end
+
+project = user.projects.find_or_create_by(name: 'Project 1', description: 'A project, yo!', link: 'www.example.com/project_1')
+if project
+  puts 'Project saved!'
+else
+  puts 'Failed to create Project'
+end
+
+post = user.posts.find_or_create_by(title: 'First Post', body: 'This is a post')
+if post
+  puts 'Post created!'
+else
+  puts 'Failed to create Post'
+end 
